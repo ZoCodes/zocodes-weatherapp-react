793edf9665dd0a1df8e228a87b354afd681fc1b0
ZoCodes <zoeblogg91@gmail.com>
2021-10-10 19:36:12 +0100

    Created AJAX call for main city weather

--- src/Form.js
+++ src/Form.js
@@ -1,19 +1,8 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 
 import "./Form.css";
 
 export default function Form() {
-  function handleAxiosResponse(response) {
-    console.log(response.data);
-  }
-
-  let city = "london";
-  let apiKey = "e744bfafcb3c1411c3f393198d753e28";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(handleAxiosResponse);
-
   return (
     <div className="form">
       <form id="city-form" className="mt-2 mb-4">
--- src/MainWeather.js
+++ src/MainWeather.js
@@ -1,49 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./MainWeather.css";
 
 export default function WeatherDescription() {
-  return (
-    <div className="main-weather">
-      <ul>
-        <div className="row no-gutters">
-          <div className="col-7">
-            <li className="main-weather-description" id="weather-description">
-              rain rain go away
-            </li>
-            <li className="humidity-stat">
-              Humidity: <span id="humidity"></span>%
-            </li>
-            <li className="wind-stat">
-              Wind: <span id="wind"></span> km/h
-            </li>
-          </div>
+  let [populated, setPopulated] = useState(false);
+  let [temperature, setTemperature] = useState(null);
 
-          <div className="col-md">
-            <li className="main-temperature">
-              <span id="temperature">15</span>
-              {/*eslint-disable-next-line*/}
-              <a href="#" id="celsius-link" className="units active">
-                °C
-              </a>
-              <span className="unit-break"> | </span>
-              {/*eslint-disable-next-line*/}
-              <a href="#" id="fahrenheit-link" className="units">
-                °F
-              </a>{" "}
-            </li>
-            <li>
-              <img
-                src="./media/09d.png"
-                className="main-weather-img image-fluid"
-                id="main-icon"
-                alt="weather"
-              />
-            </li>
-          </div>
+  function handleAxiosResponse(response) {
+    setTemperature(response.data.main.temp);
+    setPopulated(true);
+  }
 
-          <div className="col"></div>
-        </div>
-      </ul>
-    </div>
-  );
+  if (populated) {
+    return (
+      <div className="main-weather">
+        <ul>
+          <div className="row no-gutters">
+            <div className="col-7">
+              <li className="main-weather-description" id="weather-description">
+                rain rain go away
+              </li>
+              <li className="humidity-stat">
+                Humidity: <span id="humidity"></span>%
+              </li>
+              <li className="wind-stat">
+                Wind: <span id="wind"></span> km/h
+              </li>
+            </div>
+
+            <div className="col-md">
+              <li className="main-temperature">
+                <span id="temperature">{Math.round(temperature)}</span>
+                {/*eslint-disable-next-line*/}
+                <a href="#" id="celsius-link" className="units active">
+                  °C
+                </a>
+                <span className="unit-break"> | </span>
+                {/*eslint-disable-next-line*/}
+                <a href="#" id="fahrenheit-link" className="units">
+                  °F
+                </a>{" "}
+              </li>
+              <li>
+                <img
+                  src="./media/09d.png"
+                  className="main-weather-img image-fluid"
+                  id="main-icon"
+                  alt="weather"
+                />
+              </li>
+            </div>
+
+            <div className="col"></div>
+          </div>
+        </ul>
+      </div>
+    );
+  } else {
+    let city = "leeds";
+    let apiKey = "e744bfafcb3c1411c3f393198d753e28";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleAxiosResponse);
+
+    return "Loading Weather App...";
+  }
 }
